11d80a1becdf0cfa448d5c4d269c1a7ef4dfb3d4
mumalshekhawat <mumalshe7@gmail.com>
2023-05-31 22:49:07 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import RandomJokes from './RandomJokes';
+import UseEffect from './UseEffect';
 
 function App() {
   return (
     <div className="App">
       <h1 id='first'>Get New Jokes</h1>
       <RandomJokes />
+      <UseEffect/>
     </div>
   );
 }
